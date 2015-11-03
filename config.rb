55b2a5551aa21ba68fadfc615ecc6c8ff5ca3a87
Chris Toomey <chris@ctoomey.com>
2015-11-02 20:04:13 -0500

    Gzip and fingerprint assets

--- config.rb
+++ config.rb
@@ -9,5 +9,14 @@ set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
 set :images_dir, 'images'
 
+configure :build do
+  activate :gzip
+  activate :minify_css
+  activate :minify_javascript
+  activate :asset_hash
+end
+
 activate :dotenv
-activate :s3_sync
+activate :s3_sync do |s3|
+  s3.prefer_gzip = true
+end
